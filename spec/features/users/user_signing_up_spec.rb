83264405afa8f2ca39a51337ceebee51b37d0f7e
Stephan Yu <Stephan.yu@gmail.com>
2016-11-13 15:51:56 +0000

    Implement user sign up and sign in feature specs

--- spec/features/users/user_signing_up_spec.rb
+++ spec/features/users/user_signing_up_spec.rb
@@ -1,19 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature 'User signs up' do 
-  scenario 'Sign up is successfull' do 
+  scenario 'with valid credentials' do 
     visit root_path
 
     click_link 'Sign up'
     fill_in 'Email', with: 'user@email.com'
     fill_in 'Password', with: 'password'
-    click_button 'Create Account'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
 
     expect(page).to have_content('You have signed up successfully')
-    expect(current_path).to eq()
-  end
-
-  scenario 'Sign up is unsuccessful' do 
-
   end
 end
