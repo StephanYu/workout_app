4ccffdd70b7ec7bd8e00a78d5b44ac2d176f36f7
Stephan Yu <Stephan.yu@gmail.com>
2016-11-14 21:22:45 +0000

    Extend listing exercises spec to display only workout of last seven days

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -6,7 +6,9 @@ RSpec.feature 'Listing Exercises' do
     login_as(@user)
     @exercise_1 = Exercise.create(duration_in_min: 20, workout: 'Crossfit WOD', workout_date: Date.today, user: @user)
     @exercise_2 = Exercise.create(duration_in_min: 10, workout: 'Freeletics WOD', workout_date: 2.days.ago, user: @user)
+    @exercise_3 = Exercise.create(duration_in_min: 10, workout: 'Jiujitsu', workout_date: 9.days.ago, user: @user)
   end
+
   scenario 'shows users workouts for the past 7 days' do 
     visit root_path
 
@@ -19,5 +21,18 @@ RSpec.feature 'Listing Exercises' do
     expect(page).to have_content(@exercise_2.duration_in_min)
     expect(page).to have_content(@exercise_2.workout)
     expect(page).to have_content(@exercise_2.workout_date)
+
+    expect(page).not_to have_content(@exercise_3.duration_in_min)
+    expect(page).not_to have_content(@exercise_3.workout)
+    expect(page).not_to have_content(@exercise_3.workout_date)
+  end
+
+  scenario 'diplays no workout message if none exist' do 
+    @user.exercises.delete_all
+
+    visit root_path
+    click_link 'My Lounge'
+
+    expect(page).to have_content('No Workouts Yet')
   end
 end
