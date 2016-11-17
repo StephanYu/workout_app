92c2b121f488018dd3d2756f58c294e66d31082b
Stephan Yue <syue@Stephans-Mac-mini.local>
2016-11-17 10:27:12 +0000

    Add first name and last name to sign up page

--- spec/features/users/user_signing_up_spec.rb
+++ spec/features/users/user_signing_up_spec.rb
@@ -5,11 +5,16 @@ RSpec.feature 'User signs up' do
     visit root_path
 
     click_link 'Sign up'
+    fill_in 'First name', with: 'Adam'
+    fill_in 'Last name', with: 'Test'
     fill_in 'Email', with: 'user@email.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
     click_button 'Sign up'
 
     expect(page).to have_content('You have signed up successfully')
-  end
+
+    visit root_path
+    expect(page).to have_content('Adam Test')
+  end  
 end
